1eba78211314bea409533c1c564a9bf50cdc159b
kiran2701 <gaurdian.kiran@gmail.com>
2024-02-15 12:32:16 +0000

    Implemented GetItem() in Object Pool

---
+++ Assets/Scripts/Utilities/GenericObjectPool.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericObjectPool<T> where T : class
+{
+    private List<PooledItem> pooledItems = new List<PooledItem>();
+    
+    protected T GetItem()
+    {
+        if(pooledItems.Count > 0)
+        {
+            PooledItem itemInPool = pooledItems.Find(i => !i.isUsed);
+
+            if(itemInPool != null)
+            {
+                itemInPool.isUsed = true;
+                return itemInPool.itemController;
+            }
+        }
+
+        return CreateNewPooledItem();
+    }
+
+    private T CreateNewPooledItem()
+    {
+        PooledItem pooledItem = new PooledItem();
+        pooledItem.itemController = CreateNewItemController();
+        pooledItem.isUsed = true;
+
+        pooledItems.Add(pooledItem);
+
+        return pooledItem.itemController;
+    }
+
+    protected virtual T CreateNewItemController()
+    {
+        //This is to be implemented in child class to create appropriate controller
+        throw new NotImplementedException("Child class has not implemented Create New Item Controller");
+    }
+
+    public class PooledItem
+    {
+        public bool isUsed;
+        public T itemController;
+    }
+}
